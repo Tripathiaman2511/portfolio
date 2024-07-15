a78e9518b2dc1d5b3d2341032790da67d5232e2b
Aman Tripathi <iamtripathi.25@gmail.com>
2024-07-15 22:00:08 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
         < motion.h1 className=" ease-in  font-Outward text-[70vw] md:text-[43vw]   pt-[7vw] lg:text-[42vw] leading-[50vw] md:leading-[30vw] lg:leading-[25vw] lg:pt-[12vh]" initial={{ y: '100%',scale:0}}   animate={{  y: 0,scale:1 }} transition={{ duration: 0.8 }}>AMAN TRIPATHI</motion.h1>
         </motion.div>
         
-        < motion.h1 className=" ease-in font-Inter text-[7vw] md:text-[4vw] -mt-[2vh] md:mt-0 lg:text-[3vw] pl-[1vw] " initial={{  x: '-100%',scale:0 }}   animate={{ x: 0,scale:1 }} transition={{ duration: 1.5 }}>Java Developer @TCS</motion.h1>
+       
         
         <motion.p initial={{opacity:0}} animate={{opacity:1}} transition={{duration:1,delay:1}}  className="font-Inter pl-[1vw]  text-[4vw] sm:text-[3.5vw] md:text-[3vw] lg:text-[2.5vw] xl:text-[1.5vw]  sm:leading-[3.5vw] md:leading-[3vw] lg:leading-[2.5vw] xl:leading-[2vw] sm:w-[70vw] lg:w-[50vw] leading-[5vw]  pt-[10vw] md:pt-0">I am a Software Developer with a passion for building innovative applications that push the boundaries of what's possible.</motion.p>    
         {/* <div className=' overflow-hidden '>
