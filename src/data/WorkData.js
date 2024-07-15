882856516c24a26aab68333588e16ea92f1ffdda
Aman Tripathi <iamtripathi.25@gmail.com>
2024-07-15 21:56:03 +0530

    Update WorkData.js

--- src/data/WorkData.js
+++ src/data/WorkData.js
@@ -1,25 +1,31 @@
 export const WorkData=[
     {
+        'position':'Java Developer',
+        'company':'Tata Consultancy Services',
+        'smallDesc':'Developed and deployed robust scheduling and listener mechanisms within a Java-based REST API for a government initiative, leveraging Hibernate for efficient data management, resulting in a 30% increase in data processing efficiency and enhanced system responsiveness.',
+        'date':'FEB 2024'
+        
+    },
+    {
         'position':'UI Designer',
         'company':'Exposys Data Labs',
         'smallDesc':'Worked as a UI/UX Designer Intern. Designed Mobile Application of Internship Website of the Company. Developed 2 whole Design of the Application. Performed WireFrames and lots of animation.',
-        'date':'JULY 2021',
-        'image':'abstractTecture.svg'
+        'date':'JUL 2021'
+        
     },
     {
         'position':'Frontend Developer Intern',
         'company':'Agriyaan Technologies',
         'smallDesc':'Worked on a startup project by contributing in UI/UX Design and developed the Frontend part of it. Designed multiple pages using Figma software. Working with Django Backend Developer was a new lesson for me.',
-        'date':'OCTOBER 2021',
-        'image':'abstractTecture.svg'
+        'date':'OCT 2021'
+       
 
     },
     {
         'position':'Frontend Developer',
         'company':'Hackathon: Error-404 0.5',
         'smallDesc':'2 Days and 1 Night long hackathon organized by M.H. Saboo Siddik College of Engineering. The task was to develop a Decentraized Doctor Consultation WebApp using Blockchain.',
-        'date':'MARCH 2023',
-        'image':'abstractTecture.svg'
+        'date':'MAR 2023'
     
     },
     
